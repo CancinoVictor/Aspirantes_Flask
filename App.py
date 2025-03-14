e38d659a63c913e417f92e0804a4b657cb821241
Victor Cancino Hernandez <victor.izayoi@gmail.com>
2025-03-14 01:19:05 -0600

    App route

--- App.py
+++ App.py
@@ -2,11 +2,8 @@ from flask import Flask,render_template
  
 app = Flask(__name__)
  
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
 
-@app.route("/aspirante")
+@app.route("/")
 def add_aspirante():
     return render_template("aspirante.html")
 
