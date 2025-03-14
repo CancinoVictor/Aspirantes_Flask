0615a2800f58f7c8b2a5146cc72fdbd5b06a6a56
Victor Cancino Hernandez <victor.izayoi@gmail.com>
2025-03-14 01:24:22 -0600

    Agregando archivo .gitignore

--- setup.py
+++
@@ -1,4 +0,0 @@
-from setuptools import setup, find_packages
-setup(name='web',version='1.0',packages=find_packages())
- 
- 
--- test/test_Database.py
+++
@@ -1,4 +0,0 @@
-from model.package_model.Database import DB  # Importa la clase DB
-
-# Crea una instancia de la clase DB
-db = DB()
